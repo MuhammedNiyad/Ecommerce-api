a77b6655bd8f9d3a0622eb5e4bd411569d24da82
MuhammedNiyad <niyadpunniyath007@gmail.com>
2023-12-13 17:05:24 +0530

    created verify token functionality

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
 const CryptoJS = require("crypto-js"); /* Cryptojs for change user password to hash type..! */
+const jwt = require('jsonwebtoken');
 
 // REGISTER..!
 router.post("/register", async (req, res) => {
@@ -26,16 +27,21 @@ router.post("/login", async (req, res) => {
         const user = await User.findOne({ username: req.body.username });
         if (!user) {
             return res.status(401).json("Wrong credentials..!");
-        }
+        };
 
         const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC).toString(CryptoJS.enc.Utf8);
         if (hashedPassword !== req.body.password) {
             return res.status(401).json("Wrong credentials..!");
-        }
+        };
+
+        const access_token = jwt.sign({         /*Creating JSON WEB TOKEN  */
+            id: user._id,
+            isAdmin:user.isAdmin,
+        }, process.env.JWT_SEC);
 
         const { password, ...others } = user._doc;
         console.log(others);
-        res.status(200).json(others);
+        res.status(200).json({others, access_token});
     } catch (error) {
         res.status(500).json(error);
     }
