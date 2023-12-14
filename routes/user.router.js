01725c9f844ede77b8249b8209df3d8c5f00c7e8
MuhammedNiyad <niyadpunniyath007@gmail.com>
2023-12-14 15:23:26 +0530

    JWT token verifying and user updation fationality created

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require("./verifyToken")
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const User = require("../models/User.model");
 
-router.put("/:id", verifyToken);
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    console.log("in up usser")
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC);
+    }
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+
+            $set: req.body
+        }, { new: true });
+        return res.status(200).json(updatedUser);
+    } catch (error) {
+        return res.status(500).json(error);
+    };
+});
 
 
 
